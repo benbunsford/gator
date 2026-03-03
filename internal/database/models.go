5aa0d0570ada8c1f016a68d202d3330486ca609e
benbunsford <benbridgford@gmail.com>
2026-03-02 18:59:53 -0800

    add feeds table

--- internal/database/models.go
+++ internal/database/models.go
@@ -10,6 +10,14 @@ import (
 	"github.com/google/uuid"
 )
 
+type Feed struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Url       string
+}
+
 type User struct {
 	ID        uuid.UUID
 	CreatedAt time.Time
